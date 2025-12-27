e6ebbf2561551e6b6de7ed8a5f502cea9d0a6661
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2025-12-27 12:49:36 +0530

    first commit

---
+++ lib/core/config/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xff42C83C);
+  static const lightBackgroud = Color(0xfff2f2f2);
+  static const darkBackgroud = Color(0xff0d0c0c);
+  static const grey = Color(0xffbebebe);
+  static const darkGrey = Color(0xff343434);
+}
